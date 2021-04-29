08190facff0c37058d66ee1db1954430e54c7f0b
Buuxd <37423146+Buuxd@users.noreply.github.com>
2021-04-29 13:13:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
-localStorage.setItem("yo", "hey");
 
 
 
+document.getElementById("html_box").addEventListener("input", update);
+document.getElementById("css_box").addEventListener("input", update);
 
 function input()
 {
@@ -11,6 +12,12 @@ var css_entry = document.getElementById("css_box").value;
 document.getElementById("main").innerHTML = "<style>" + css_entry + "</style>" + html_entry;
 }
 
+
+function update() {
+  xp.html = html_box.value; // get updated user values
+  xp.css = css_box.value;
+  xp.input(); // render the new html area
+}
 
 
 
