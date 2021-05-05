9773d5489de3e62e4782011935cbcd665245d092
Buuxd <37423146+Buuxd@users.noreply.github.com>
2021-05-05 13:10:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function input()
 {
 var html_entry = document.getElementById("html_box").value;
-var css_entry = document.getElementById("css_box").value;
-document.getElementById("main").innerHTML = "<style>" + css_entry + "</style>" + html_entry;
-localStorage.setItem("GRADE", html_entry);
-localStorage.setItem("BPLZ", css_entry);
-
-
+		var css_entry = document.getElementById("css_box").value;
+			document.getElementById("main").innerHTML = "<style>" + css_entry + "</style>" + html_entry;
+			localStorage.setItem("html", html_entry);
+		localStorage.setItem("css", css_entry);
+window.addEventListener("load", recoverPreviousPage)
+function onload(){
+	var relthml = localStorage.getItem("html");
+	var relcss = localStorage.getItem("css");
+	document.getElementById("html_box").value = relthml;
+	document.getElementById("css_box").value = relcss;
+	
+	
+	
+	}
 }
+
+
