cb362098425c3f39d6f8beb58de88b885061ee56
Buuxd <37423146+Buuxd@users.noreply.github.com>
2021-05-07 11:45:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ var html_entry = document.getElementById("html_box").value;
 			document.getElementById("main").innerHTML = "<style>" + css_entry + "</style>" + html_entry;
 			localStorage.setItem("html", html_entry);
 		localStorage.setItem("css", css_entry);
-window.addEventListener("load", recoverPreviousPage)
+		window.addEventListener("load", onLoad);
+			
+	}
 function onload(){
 	var relthml = localStorage.getItem("html");
 	var relcss = localStorage.getItem("css");
-	document.getElementById("html_box").value = relthml;
-	document.getElementById("css_box").value = relcss;
-	
-	
-	
+	if ("html" in localStorage) {
+		html_entry = relthml;
+	} else {
+		alert("lol ur dumb");
 	}
 }
-
-
